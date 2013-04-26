0569e438d22f71b2f0e3ed40e7b1bf5eadfbbea8
Anderson SP <andersonsp24@gmail.com>
2013-04-26 17:35:27 -0430

    first version towards an tiny io interpreter

--- lys.c
+++ lys.c
@@ -1,77 +1,307 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <setjmp.h>
-#include <ctype.h>
-#include <stdint.h>
-#include <stdarg.h>
-#include <assert.h>
 #include "lys.h"
 
-/*
- * Interpreter for Lys scripting language.
- * BUILD: gcc lys.c -o lys
- * USAGE: lys file.lys
- *
- * The grammar of Lys in EBNF is:
- *
- *  <comments> ::= "#" ... "\n" .
- *  <program> ::= {<statement>} .
- *  <statement> ::= "if" <paren_expr> <statement>
- *                  | "if" <paren_expr> <statement> "else" <statement>
- *                  | "while" <paren_expr> <statement>
- *                  | "do" <statement> "while" <paren_expr> ";"
- *                  | "{" { <statement> } "}"
- *                  | <expr> ";"
- *                  | ";" .
- *  <paren_expr> ::= "(" <expr> ")" .
- *  <expr> ::= <test> | <id> "=" <expr> .
- *  <test> ::= <sum> | <sum> "<" <sum> | <sum> ">" <sum> .
- *  <sum> ::= <term> | <sum> "+" <term> | <sum> "-" <term> .
- *  <term> ::= <id> | <int> | <paren_expr> .
- *  <id> ::= "a" | "b" | "c" | "d" | ... | "z" .
- *  <int> ::= <an_unsigned_decimal_integer> .
- */
-
-// Taken from Lua  ---- Instruction encoding for a "simple" register based VM
-#define SIZE_C    9
-#define SIZE_B    9
-#define SIZE_Bx   (SIZE_C + SIZE_B)
-#define SIZE_A    9
-#define SIZE_OP   5
-
-#define POS_OP    0
-#define POS_A   (POS_OP + SIZE_OP)
-#define POS_C   (POS_A + SIZE_A)
-#define POS_B   (POS_C + SIZE_C)
-#define POS_Bx    POS_C
-
-#define MAXARG_Bx   ((1<<SIZE_Bx)-1)
-#define MAXARG_sBx  (MAXARG_Bx>>1)
-
-#define CREATE_ABC(o,a,b,c) ( (((uint32_t) o) <<POS_OP) | (((uint32_t) a) <<POS_A) \
-                              | (((uint32_t) b) <<POS_B) | (((uint32_t) c) <<POS_C) )
-#define CREATE_ABx(o,a,bc) ( (((uint32_t) o)<<POS_OP) | (((uint32_t) a)<<POS_A) | (((uint32_t) bc)<<POS_Bx) )
-#define CREATE_AsBx(o,a,bc) ( (((uint32_t) o)<<POS_OP) | (((uint32_t) a)<<POS_A) | (((uint32_t) (bc+MAXARG_sBx))<<POS_Bx) )
-
-#define MASK1(n,p)    ((~((~(uint32_t)0)<<n))<<p)
-#define GET_OPCODE(i) ( (int) (((i)>>POS_OP) & MASK1(SIZE_OP,0)))
-#define GETARG_A(i)   ( ((int) ((i)>>POS_A) & MASK1(SIZE_A,0)))
-#define GETARG_B(i)   ( ((int) ((i)>>POS_B) & MASK1(SIZE_B,0)))
-#define GETARG_C(i)   ( ((int) ((i)>>POS_C) & MASK1(SIZE_C,0)))
-#define GETARG_Bx(i)  ( ((int) ((i)>>POS_Bx) & MASK1(SIZE_Bx,0)))
-#define GETARG_sBx(i) (GETARG_Bx(i)-MAXARG_sBx)
-
-#define BITRK   (1 << (SIZE_B - 1))
-#define ISK(x)    ((x) & BITRK)
-#define INDEXK(r) ((int)(r) & ~BITRK)
-#define MAXINDEXRK  (BITRK - 1)
-#define RKASK(x)  ((x) | BITRK)
+// * Interpreter for Lys scripting language.
+// * BUILD: gcc lys.c -o lys
+// * USAGE: lys file.lys
+// *
+// * The grammar of Lys in EBNF is:
+// *
+// * exp        ::= { message | terminator }
+// * message    ::= symbol [arguments]
+// * arguments  ::= "(" [exp [ { "," exp } ]] ")"
+// * symbol     ::= identifier | number | string
+// * terminator ::= "\n" | ";"
 
 //
 // Object defs
 //
-typedef int Object;
+#define MCACHE 1    /* nonzero to enable global method cache        */
+#define POINTER_INT long
+#define POINTER_UINT unsigned long
+
+typedef struct _Vtable Vtable;
+typedef struct _Object Object;
+typedef struct _Closure Closure;
+typedef struct _Symbol Symbol;
+
+typedef Object *(*imp_t)(Closure *closure, Object *receiver, ...);
+
+struct _Vtable {
+  Vtable  *_vt[0];
+  int size, tally;
+  Object **keys, **values;
+  Vtable  *parent;
+};
+
+struct _Object {
+  Vtable *_vt[0];
+};
+
+struct _Closure {
+  Vtable *_vt[0];
+  imp_t      method;
+  Object *data;
+};
+
+struct _Symbol {
+  Vtable *_vt[0];
+  char *string;
+};
+
+Vtable *SymbolList= NULL;
+
+Vtable *vtable_vt;
+Vtable *object_vt;
+Vtable *symbol_vt;
+Vtable *closure_vt;
+
+Object *s_add_method = NULL;
+Object *s_allocate  = NULL;
+Object *s_delegated = NULL;
+Object *s_lookup    = NULL;
+
+inline void *alloc(size_t size) {
+  Vtable **ppvt= (Vtable **)calloc(1, sizeof(Vtable *) + size);
+  return (void *)(ppvt + 1);
+}
+
+Object *symbol_new(char *string) {
+  Symbol *symbol = (Symbol *)alloc(sizeof(Symbol));
+  symbol->_vt[-1] = symbol_vt;
+  symbol->string = strdup(string);
+  return (Object *)symbol;
+}
+
+Object *closure_new(imp_t method, Object *data) {
+  Closure *closure = (Closure *)alloc(sizeof(Closure));
+  closure->_vt[-1] = closure_vt;
+  closure->method  = method;
+  closure->data    = data;
+  return (Object *)closure;
+}
+
+Object *vtable_lookup(Closure *closure, Vtable *self, Object *key);
+
+# define send(RCV, MSG, ARGS...) ({             \
+      Object   *r = (Object *)(RCV);   \
+      static Vtable   *prevVT  = NULL;          \
+      static Closure  *closure = NULL;          \
+      register Vtable *thisVT  = r->_vt[-1];     \
+      thisVT == prevVT ? closure : (prevVT  = thisVT, closure = bind(r, (MSG)));               \
+      closure->method(closure, r, ##ARGS);          \
+    })
+
+#if MCACHE
+struct entry {
+  Vtable  *vtable;
+  Object  *selector;
+  Closure *closure;
+} MethodCache[8192];
+#endif
+
+Closure *bind(Object *rcv, Object *msg) {
+  Closure *c;
+  Vtable  *vt = rcv->_vt[-1];
+#if MCACHE
+  struct entry   *cl = MethodCache + ((((POINTER_UINT)vt << 2) ^ ((POINTER_UINT)msg >> 3)) & ((sizeof(MethodCache) / sizeof(struct entry)) - 1));
+  if (cl->vtable == vt && cl->selector == msg) return cl->closure;
+#endif
+  c = ((msg == s_lookup) && (rcv == (Object *)vtable_vt))
+    ? (Closure *)vtable_lookup(0, vt, msg)
+    : (Closure *)send(vt, s_lookup, msg);
+#if MCACHE
+  cl->vtable   = vt;
+  cl->selector = msg;
+  cl->closure  = c;
+#endif
+  return c;
+}
+
+Vtable *vtable_delegated(Closure *closure, Vtable *self) {
+    Vtable *child= (Vtable *)alloc(sizeof(Vtable));
+    child->_vt[-1] = self ? self->_vt[-1] : 0;
+    child->size    = 2;
+    child->tally   = 0;
+    child->keys    = (Object **)calloc(child->size, sizeof(Object *));
+    child->values  = (Object **)calloc(child->size, sizeof(Object *));
+    child->parent  = self;
+    return child;
+}
+
+Object *vtable_allocate(Closure *closure, Vtable *self, int payloadSize) {
+  Object *object = (Object *)alloc(payloadSize);
+  object->_vt[-1] = self;
+  return object;
+}
+
+imp_t vtable_add_method(Closure *closure, Vtable *self, Object *key, imp_t method) {
+    int i;
+    for( i = 0; i < self->tally; ++i ) {
+        if( key == self->keys[i] ) return ((Closure *)self->values[i])->method = method;
+    }
+    if( self->tally == self->size ) {
+        self->size  *= 2;
+        self->keys   = (Object **)realloc(self->keys,   sizeof(Object *) * self->size);
+        self->values = (Object **)realloc(self->values, sizeof(Object *) * self->size);
+    }
+    self->keys[ self->tally ] = key;
+    self->values[ self->tally++ ] = closure_new(method, 0);
+    return method;
+}
+
+Object *vtable_lookup(Closure *closure, Vtable *self, Object *key) {
+    int i;
+    for( i = 0;  i < self->tally; ++i) {
+        if (key == self->keys[i]) return self->values[i];
+    }
+    if( self->parent ) return send(self->parent, s_lookup, key);
+    fprintf(stderr, "lookup failed %p %s\n", self, ((Symbol *)key)->string);
+    return 0;
+}
+
+Object *symbol_intern(Closure *closure, Object *self, char *string) {
+    Object *symbol;
+    int i;
+    for(i = 0;  i < SymbolList->tally;  ++i) {
+        symbol = SymbolList->keys[i];
+        if(!strcmp(string, ((Symbol *)symbol)->string)) return symbol;
+    }
+    symbol = symbol_new(string);
+    vtable_add_method(0, SymbolList, symbol, 0);
+    return symbol;
+}
+
+void init(void) {
+    vtable_vt = vtable_delegated(0, 0);
+    vtable_vt->_vt[-1] = vtable_vt;
+
+    object_vt = vtable_delegated(0, 0);
+    object_vt->_vt[-1] = vtable_vt;
+    vtable_vt->parent = object_vt;
+
+    symbol_vt  = vtable_delegated(0, object_vt);
+    closure_vt = vtable_delegated(0, object_vt);
+
+    SymbolList = vtable_delegated(0, 0);
+
+    s_lookup = symbol_intern(0, 0, "lookup");
+    s_add_method = symbol_intern(0, 0, "add_method");
+    s_allocate = symbol_intern(0, 0, "allocate");
+    s_delegated = symbol_intern(0, 0, "delegated");
+
+    vtable_add_method(0, vtable_vt, s_lookup, (imp_t)vtable_lookup);
+    vtable_add_method(0, vtable_vt, s_add_method, (imp_t)vtable_add_method);
+
+    send(vtable_vt, s_add_method, s_allocate, vtable_allocate);
+    send(vtable_vt, s_add_method, s_delegated, vtable_delegated);
+}
+
+//----------------------------------------------------------------
+
+Object *s_new= 0;
+Object *s_length= 0;
+
+struct Number {
+  Vtable *_vt[0];
+};
+
+Vtable *Number_vt = 0;
+Object *Number = 0;
+
+Object *Number_new(Closure *closure, struct Number *self) {
+  fprintf(stderr, "Number_new\n");
+  exit(1);
+  return 0;
+}
+
+int Number_length(Closure *closure, struct Number *self) {
+  fprintf(stderr, "Number has no length\n");
+  exit(1);
+  return 0;
+}
+
+struct String {
+  Vtable *_vt[0];
+  int length;
+  char *chars;
+};
+
+Vtable *String_vt = 0;
+Object *String = 0;
+
+Object *String_new(Closure *closure, struct String *self, int size) {
+  struct String *clone = (struct String *)send(self->_vt[-1], s_allocate, sizeof(struct String));
+  clone->length = size;
+  clone->chars  = (char *)malloc(size);
+  return (Object *)clone;
+}
+
+int String_length(Closure *closure, struct String *self) {
+  return self->length;
+}
+
+struct Vector {
+  Vtable *_vt[0];
+  int length;
+  Object *contents;
+};
+
+Vtable *Vector_vt = 0;
+Object *Vector = 0;
+
+Object *Vector_new(Closure *closure, struct Vector *self, int size) {
+  struct Vector *clone = (struct Vector *)send(self->_vt[-1], s_allocate, sizeof(struct Vector));
+  clone->length   = size;
+  clone->contents = (Object *)calloc(size, sizeof(Object *));
+  return (Object *)clone;
+}
+
+int Vector_length(Closure *closure, struct Vector *self) {
+  return self->length;
+}
+
+void init2(void) {
+  s_new    = symbol_intern(0, 0, "new");
+  s_length = symbol_intern(0, 0, "length");
+
+  Number_vt = (Vtable *)send(object_vt, s_delegated);
+  String_vt = (Vtable *)send(object_vt, s_delegated);
+  Vector_vt = (Vtable *)send(object_vt, s_delegated);
+
+  send(Number_vt, s_add_method, s_new, Number_new);
+  send(String_vt, s_add_method, s_new, String_new);
+  send(Vector_vt, s_add_method, s_new, Vector_new);
+
+  send(Number_vt, s_add_method, s_length, Number_length);
+  send(String_vt, s_add_method, s_length, String_length);
+  send(Vector_vt, s_add_method, s_length, Vector_length);
+
+  Number = send(Number_vt, s_allocate, 0);
+  String = send(String_vt, s_allocate, 0);
+  Vector = send(Vector_vt, s_allocate, 0);
+}
+
+//----------------------------------------------------------------
+
+void doit(void) {
+    int i, j;
+
+    Object *a = send(String, s_new, 1);
+    Object *b = send(Vector, s_new, 3);
+
+    for( i = 0, j = 0;  i < 200;  ++i ) {
+        j += (POINTER_INT)send(a, s_length) + (POINTER_INT)send(b, s_length);
+    }
+
+    printf("total %d\n", j);
+}
+
+int main() {
+  init();
+  init2();
+  doit();
+  return 0;
+}
 
 //
 // Lexer.
@@ -104,7 +334,7 @@ void next_ch() {
 }
 
 void next_sym() {
-    again: switch (ch) {
+    again: switch(ch) {
         case ' ': case '\t': case '\r':
             next_ch();
             goto again;
@@ -153,276 +383,3 @@ void next_sym() {
     }
 }
 
-//
-// Parser & Compiler.
-//
-enum {
-    OP_HALT, OP_MOVE, OP_LOADK, OP_LOADNFT, OP_GETGLOBAL, OP_SETGLOBAL,
-    OP_ADD, OP_SUB, OP_MUL, OP_DIV, OP_MOD, OP_POW, OP_UNM, OP_NOT,
-    OP_JMP, OP_JZ, OP_AND, OP_OR, OP_EQ, OP_NEQ, OP_LT, OP_LE, OP_GT, OP_GE,
-    OP_CALL, OP_RET
-};
-
-#define SIZEK 512
-#define SIZEC 1024*1024
-
-Object constants[ SIZEK ];
-uint32_t code[ SIZEC ] = {0};
-int topk = 0, topc = 0, sp = 26;
-
-static int geti() {
-    // maybe grow the size of code
-    topc++;
-    return topc-1;
-}
-
-static int getk() {
-    // maybe grow the size of constants area
-    topk++;
-    return topk-1;
-}
-
-static int paren_expr(); // forward declaration
-
-static int term() {  // <term> ::= <id> | <int> | <paren_expr>
-    int a = 0;
-    last_sym = sym;
-    if( sym == ID ) {
-        a = id_name[0]-'a';
-        next_sym();
-    } else if( sym == INT ) {
-        a = getk();
-        constants[ a ] = int_val;
-        next_sym();
-        a = RKASK( a );
-    } else {
-        a = paren_expr( 0 ); // don't eval expression in parents ?
-    }
-    return a;
-}
-
-static int sum() { // <sum> ::= <term> | <sum> "+" <term> | <sum> "-" <term>
-    int i, b, c, a = term();
-    while( sym == '+' || sym == '-' ) {
-        b = a;
-        a = sp++;
-        i = (sym == '+' ? OP_ADD : OP_SUB);
-        next_sym();
-        c = term();
-        last_sym = '+';
-        int x = geti();
-        code[x] = CREATE_ABC( i, a, b, c );
-    }
-    return a;
-}
-
-static int test() { // <test> ::= <sum> | <sum> "<" <sum>
-    int i, b, c, a = sum();
-    if( sym == '<' || sym == '>' ) { // FIXME ??
-        b = a;
-        a = sp++;       //find a temporal register to store the result
-        i = ( sym == '<' ? OP_LT : OP_GT );
-        next_sym();
-        c = sum();
-        last_sym = '<';
-        int x = geti();
-        code[x] = CREATE_ABC( i, a, b, c );
-    }
-    return a;
-}
-
-static int expr() {  // <expr> ::= <test> | <id> "=" <expr>
-    int b, a, saved_sp = sp;
-    if( sym == ID ) {
-        a = test();
-        if( last_sym == ID && sym == '=' ) {
-            next_sym();
-            b = expr();
-            int x = geti();
-            code[x] = CREATE_ABC( OP_MOVE, a, b, 0 );
-        }
-    } else {
-        a = test();
-    }
-
-    sp = saved_sp;
-    return a;
-}
-
-static int paren_expr( int eval ) { // <paren_expr> ::= "(" <expr> ")"
-    int a, b;
-
-    if( sym == '(') next_sym();
-    else syntax_error( "expected '('\n" );
-    a = expr();
-
-    if( ISK(a) && eval ) {
-        int x = geti();
-        b = a;
-        a = sp++;
-        code[x] = CREATE_ABx( OP_LOADK, a, b );
-    }
-
-    if( sym == ')' ) next_sym();
-    else syntax_error( "expected ')'\n" );
-
-    return a;
-}
-
-static int statement() {
-    int x, b, c, a = 0;
-    float aux;
-    int saved_sp;
-
-    if( sym == IF_SYM ) {  // "if" <paren_expr> <statement>
-        next_sym();
-        a = paren_expr( 1 );
-        x = geti();
-        c = statement();
-        code[x] = CREATE_AsBx( OP_JZ, a, topc-x-1 );
-
-        if( sym == ELSE_SYM ) { // ... "else" <statement>
-            code[x] = CREATE_AsBx( OP_JZ, a, topc-x );
-            x = geti();
-            next_sym();
-            statement();
-            code[x] = CREATE_AsBx( OP_JMP, a, topc-x-1 );
-        }
-
-    } else if( sym == WHILE_SYM ) {  // "while" <paren_expr> <statement>
-        next_sym();
-        b = topc;
-        a = paren_expr( 1 );
-        c = geti();
-        statement();
-        x = geti();
-        code[x] = CREATE_AsBx( OP_JMP, 0, b-topc );
-        code[c] = CREATE_AsBx( OP_JZ, a, topc-c-1 );
-
-    } else if( sym == DO_SYM ) {  // "do" <statement> "while" <paren_expr> ";"
-        next_sym();
-        b = topc;
-        statement();
-        if( sym == WHILE_SYM ) next_sym();
-        else syntax_error( "expected 'while' token\n" );
-        a = paren_expr( 1 );
-        if( sym == ';' ) next_sym();
-        else syntax_error( "expected ';'\n" );
-        x = geti();
-        code[x] = CREATE_AsBx( OP_JZ, a, 1 );
-        x = geti();
-        code[x] = CREATE_AsBx( OP_JMP, 0, b-topc );
-
-    } else if( sym == ';' ) { // ";"
-        next_sym();
-
-    } else if( sym == '{' ) {  // "{" { <statement> } "}"
-        saved_sp = sp;
-
-        next_sym();
-        while( sym != '}' ) statement();
-        next_sym();
-
-        sp = saved_sp;
-
-    } else { // <expr> ";"
-        expr();
-        if( sym == ';' ) next_sym();
-        else syntax_error( "in expression expected ';'\n" );
-    }
-
-    return a;
-}
-
-static int program() { // <program> ::= <statement>
-    int a;
-    next_sym();
-    while( sym != EOI ) a = statement();
-    if( sym != EOI ) syntax_error( "expected EOF\n" );
-    return a;
-}
-
-//
-// Virtual machine.
-//
-// Object globals[26];
-Object stack[1024];
-
-#define RKB(i) ( ISK(GETARG_B(i)) ? &constants[INDEXK(GETARG_B(i))] : &stack[GETARG_B(i)] )
-#define RKC(i) ( ISK(GETARG_C(i)) ? &constants[INDEXK(GETARG_C(i))] : &stack[GETARG_C(i)] )
-
-int ip = 0;
-void run() {
-    uint32_t i, op, done = 0;
-    int a, b, c, e;       // opcode args
-    Object *rb, *rc, *ob; // we are working only with integers for now
-
-    do {
-        i = code[ ip++ ];
-        op = GET_OPCODE(i);
-        a = GETARG_A(i);
-        if( op >= OP_MOVE || op <= OP_AND ) {
-            rb = RKB(i);
-            rc = RKC(i);
-        }
-
-        switch( op ) {
-            case OP_HALT:
-                done = 1;
-                break;
-            case OP_LOADK:
-                stack[a] = constants[GETARG_Bx(i)];
-                break;
-            case OP_MOVE:
-                stack[ a ] = *rb;
-                break;
-            case OP_ADD:
-                stack[ a ] = *rb + *rc;
-                break;
-            case OP_SUB:
-                stack[ a ] = *rb - *rc;
-                break;
-            case OP_LT:
-                stack[ a ] = (*rb < *rc);
-                break;
-            case OP_GT:
-                stack[ a ] = (*rb < *rc);
-                break;
-            case OP_JMP:
-                ip += GETARG_sBx(i);
-                break;
-            case OP_JZ:
-                if( stack[ a ] == 0  )
-                    ip += GETARG_sBx(i);
-                break;
-        }
-
-    } while( !done && ip <= SIZEC );
-}
-
-void usage(){
-    printf("USAGE:\n  lys file.lys\n");
-}
-
-//
-// Main program.
-//
-int main( int argc, char**argv ) {
-    if( argc < 2 ) {
-        usage();
-        return 0;
-    }
-
-    fin = fopen( argv[1], "r" );
-    program();
-
-    int i;
-    for( i=0; i<26; i++ ) stack[i] = 0;
-    run();
-    for( i=0; i<26; i++ ) {
-        if( stack[i] != 0 )
-            printf( "%c = %d\n", 'a'+i, stack[i] );
-    }
-
-  return 0;
-}
---
+++ lys.h
@@ -0,0 +1,19 @@
+#ifndef LYS_H_DEFINED
+#define LYS_H_DEFINED
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <setjmp.h>
+#include <ctype.h>
+#include <stdint.h>
+#include <stdarg.h>
+#include <assert.h>
+
+
+
+
+
+#endif
+
+
